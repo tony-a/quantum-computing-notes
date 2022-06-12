cb4d312ee2a1535e1ce3983b7ff406c5c32965b4
Tony Askar <askar.b.tony@gmail.com>
2022-06-12 00:32:29 -0700

    jupyter notebook of code exercises from udemy course

---
+++ udemy_course/template_of_ideal_quantum_computer.py
@@ -0,0 +1,12 @@
+from qiskit import transpile
+from qiskit.providers.aer import QasmSimulator
+backend = QasmSimulator()
+qc_compiled = transpile(qc, backend)  
+job = backend.run(qc_compiled, shots=1024)
+result = job.result()
+counts = result.get_counts()
+print(counts)
+
+
+from qiskit.visualization import plot_histogram
+plot_histogram(counts)
